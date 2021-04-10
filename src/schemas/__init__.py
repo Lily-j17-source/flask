922a7ddb8d2c44a50ae7cf2d6509e16c2293ccfe
Lily-j17-source <liliana.1793@hotmail.com>
2021-04-10 16:06:49 -0500

    add models, schemas,views

---
+++ src/schemas/__init__.py
@@ -0,0 +1,13 @@
+from marshmallow import fields, Schema
+
+class BootcampListSchema(Schema):
+    """Bootcamp Schema"""
+
+    id = fields.Int(required=True)
+    name = fields.Str(required=True)
+
+class BootcampSchema(Schema):
+    """Bootcamp Schema"""
+    id = fields.Int(required=True)
+    name = fields.Str(required=True)
+    created_at = fields.DateTime(required=True)
